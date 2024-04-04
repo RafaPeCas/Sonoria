dd0aa7e18cdc45ee9d1b6007fee9b234a1177222
Rafa <blas_tois@alumnos.ilernasevilla.com>
2024-04-04 19:55:13 +0200

    Fortify installed

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Europe/Madrid',
 
     /*
     |--------------------------------------------------------------------------
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
     ])->toArray(),
 
     /*
