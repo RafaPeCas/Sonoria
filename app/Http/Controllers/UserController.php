4b34f9e461e20ef81e5b74da63d840d4371988cf
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-04-13 11:50:29 +0200

    Movemos vistos user a su carpeta y cambiamos rutas de la vista

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,19 +18,19 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => ['nullable', 'string', 'min:2', 'regex:/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/'],
             'email' => ['nullable', 'string', 'email', 'max:255', 'regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|net|org|edu|gov)$/i'],
-           
+
             'name.min' => 'El nombre debe tener mínimo :min letras.',
             'name.regex' => 'El nombre no puede contener números ni caracteres especiales.',
             'email.email' => 'El email debe ser una dirección de correo válida.',
             'email.ends_with' => 'El email debe terminar en ".com, .net, .org, .edu, .gov".',
-           
+
         ]);
-    
+
         // Si la validación falla, redireccionar con los errores
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
         }
-    
+
         $user = Auth::user();
         if ($user) {
             // Verificar si el usuario ha actualizado algún campo
@@ -41,13 +41,13 @@ class UserController extends Controller
                     $user->name = $request->name;
                     $user->email = $request->email;
                     $user->gender = $request->gender;
-                    
+
                     $user->save();
                     return redirect('/')->with('success', 'Usuario actualizado exitosamente');
                 } catch (QueryException $exception) {
                     //Si introduce email  existente le salta el mensaje de error
                     if ($exception->errorInfo[1] === 1062) {
-         
+
                     throw ValidationException::withMessages(['email' => 'El correo electrónico  ya está en uso']);
                     } else {
                         // Manejar otros errores de la base de datos
@@ -63,18 +63,18 @@ class UserController extends Controller
             return redirect()->route('login')->with('success', '');
         }
     }
-    
+
     public function edit()
     {
         $user = Auth::user();
-        
 
-        return view("userEditData", compact("user"));
+
+        return view("user/userEditData", compact("user"));
     }
-    
+
     public function seeData(){
         $user=Auth::user();
-    
-        return view("userData", compact('user'));
+
+        return view("user/userData", compact('user'));
     }
 }
