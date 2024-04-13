ceace9f18f4116d1987d43857d2592fc7ed0ac49
Rafa <blas_tois@alumnos.ilernasevilla.com>
2024-04-13 11:41:56 +0200

    reproduction panel

--- resources/views/user/_reproductionPanel.blade.php
+++ resources/views/user/_reproductionPanel.blade.php
@@ -1,37 +1,46 @@
 <section id="reproductionPanel">
     <section id="reproductionPanel">
-        @if (Auth::user()->last_song)
-            <div id="songInfo">
-                <div class="d-flex custom align-items-center h-100 pl-2">
-                    <div>
-                        <img src="" alt="">
-                    </div>
-                    <div>
-                        <h2>Absolution</h2>
-                        <p>Muse</p>
-                    </div>
+        @if (1==0)
+        <div id="songInfo">
+            <div class="d-flex custom align-items-center h-100 pl-2">
+                <div>
+                    <img class="coverImg" src="{{ $lastSong->album->image }}" alt="">
                 </div>
-            </div>
-            <div id="mediaPlayer">
-                <div class="d-flex custom align-items-center h-100 pl-2">
-                    <div>
-                        <h1>
-                        </h1>
-                    </div>
-                    <div>
+                <div>
+                    <div class="songTitle">
+                        <div>
+                            <h2>{{ $lastSong->name }}</h2>
+                        </div>
 
                     </div>
+                    <p>{{ $lastSong->album->name }}</p>
                 </div>
             </div>
+        </div>
+        <div id="mediaPlayer">
+            <div class="d-flex custom align-items-center h-100 pl-2">
+                <div>
+                    <h1>
+                    </h1>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
         @else
         <div id="songInfo">
             <div class="d-flex custom align-items-center h-100 pl-2">
                 <div>
-                    <img src="/img/cover/test.jpg" alt="">
+                    <img class="coverImg" src="{{asset("img/cover/test.jpg")}}" alt="">
                 </div>
                 <div>
-                    <h2>Absolution</h2>
-                    <p>Muse</p>
+                    <div class="songTitle">
+                        <div>
+                            <h2>Time is runnig out</h2>
+                        </div>
+                    </div>
+                    <a href="#">Absolution</a>
                 </div>
             </div>
         </div>
