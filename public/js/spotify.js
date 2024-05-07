067a935df6b758fc163e8ae64c7db3ac52fdb763
Rafa <blas_tois@alumnos.ilernasevilla.com>
2024-05-07 13:09:28 +0200

    zxasdef

--- public/js/spotify.js
+++ public/js/spotify.js
@@ -1,16 +1,15 @@
 "use"
 
 function userLoginRequest() {
-    let clientId = "e1ccac91ae2f4f3c8534f5f9b00d729c"
-    let logInUri
+    let clientId = "e1ccac91ae2f4f3c8534f5f9b00d729c";
+    let logInUri;
 
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
     params.append("redirect_uri", "http://localhost:8000/spotify");
-    params.append("scope", "user-read-private user-read-email");
+    params.append("scope", "user-read-private user-read-email user-library-read");
     params.append("show_dialog", "true");
-
 
     logInUri = `https://accounts.spotify.com/authorize?${params.toString()}`;
 
